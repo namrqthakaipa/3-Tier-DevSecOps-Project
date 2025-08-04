9caf41c8ec0b0dba677417ea9123121c948af900
namrqthakaipa <53577056+namrqthakaipa@users.noreply.github.com>
2025-08-04 11:41:38 +0530

    adding dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import UserDashboard from './pages/UserDashboard';
 import NotFound from './pages/NotFound';
 import Layout from './components/Layout';
 const GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyzABCD";
+password = "MySuperSecret123"
 
 function App() {
   return (
